e2e7b7e0ef72260a94ad6e52b3ec9ffc9890f5f0
unknown <bhavana.kommineni@walkingtree.tech>
2020-03-04 18:22:11 +0530

    provided extra info

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -27,7 +27,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find the location, Please try with another')
         } else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary +'. Day has high temperature of '+ body.daily.data[0].temperatureHigh +' and low temperrature of '+ body.daily.data[0].temperatureLow +' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
             // callback(undefined, ({
             //     Temperature: body.currently.temperature,
             //     Rain_Prob: body.currently.precipProbability
